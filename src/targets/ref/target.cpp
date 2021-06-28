ad1937c939badaceeb5928347501f1ce971ea8f9
Shucai Xiao <Shucai.Xiao@amd.com>
2021-06-28 14:56:28 -0500

    clang format

--- src/targets/ref/target.cpp
+++ src/targets/ref/target.cpp
@@ -15,33 +15,34 @@
 #include <migraphx/generate.hpp>
 #include <migraphx/normalize_ops.hpp>
 
-namespace migraphx {
-inline namespace MIGRAPHX_INLINE_NS {
-namespace ref {
-
-std::string target::name() const { return "ref"; }
-
-std::vector<pass> target::get_passes(migraphx::context&, const compile_options&) const
+namespace migraphx
 {
-    return {normalize_ops{},
-            rewrite_qdq{},
-            dead_code_elimination{},
-            eliminate_pad{},
-            dead_code_elimination{},
-            insert_pad{},
-            dead_code_elimination{},
-            rewrite_rnn{},
-            dead_code_elimination{},
-            auto_contiguous{},
-            dead_code_elimination{},
-            lowering{},
-            dead_code_elimination{}};
-}
+    inline namespace MIGRAPHX_INLINE_NS {
+    namespace ref {
 
-argument target::allocate(const shape& s) const { return fill_argument(s, 0); }
+    std::string target::name() const { return "ref"; }
 
-MIGRAPHX_REGISTER_TARGET(target);
+    std::vector<pass> target::get_passes(migraphx::context&, const compile_options&) const
+    {
+        return {normalize_ops{},
+                rewrite_qdq{},
+                dead_code_elimination{},
+                eliminate_pad{},
+                dead_code_elimination{},
+                insert_pad{},
+                dead_code_elimination{},
+                rewrite_rnn{},
+                dead_code_elimination{},
+                auto_contiguous{},
+                dead_code_elimination{},
+                lowering{},
+                dead_code_elimination{}};
+    }
 
-} // namespace ref
-} // namespace MIGRAPHX_INLINE_NS
+    argument target::allocate(const shape& s) const { return fill_argument(s, 0); }
+
+    MIGRAPHX_REGISTER_TARGET(target);
+
+    } // namespace ref
+    } // namespace MIGRAPHX_INLINE_NS
 } // namespace migraphx
