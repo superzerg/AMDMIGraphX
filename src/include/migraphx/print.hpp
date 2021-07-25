65f3d38f21dd0891ebd9307fdc9dab13c1cabf13
Shucai Xiao <Shucai.Xiao@amd.com>
2021-07-24 22:25:36 -0500

    clang format

--- src/include/migraphx/print.hpp
+++ src/include/migraphx/print.hpp
@@ -7,34 +7,38 @@
 #include <iomanip>
 #include <ostream>
 
-template<class T>
+template <class T>
 void print_vec(std::ostream& os, const std::vector<T>& vec)
 {
     std::size_t print_num = 320;
     os << "{";
     std::size_t elem_num = vec.size() > print_num ? print_num : vec.size();
-    for (std::size_t i = 0; i < elem_num; ++i)
+    for(std::size_t i = 0; i < elem_num; ++i)
     {
         os << std::setw(12) << vec[i];
-        if (i != vec.size() - 1) os << ", ";
-        if (((i + 1) % 8) == 0) os << std::endl;
+        if(i != vec.size() - 1)
+            os << ", ";
+        if(((i + 1) % 8) == 0)
+            os << std::endl;
     }
 
     std::cout << "........." << std::endl;
 
     std::size_t start = vec.size() - print_num > 0 ? vec.size() - print_num : 0;
-    start = start < print_num ? print_num : start;
-    for (std::size_t i = 0; i < elem_num; ++i)
+    start             = start < print_num ? print_num : start;
+    for(std::size_t i = 0; i < elem_num; ++i)
     {
         os << std::setw(12) << vec[i];
-        if (i != vec.size() - 1) os << ", ";
-        if (((i + 1) % 8) == 0) os << std::endl;
+        if(i != vec.size() - 1)
+            os << ", ";
+        if(((i + 1) % 8) == 0)
+            os << std::endl;
     }
     os << "}";
 }
 
-template<class T>
-std::ostream& operator << (std::ostream& os, const std::vector<T>& vec)
+template <class T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
 {
     print_vec(os, vec);
     return os;
