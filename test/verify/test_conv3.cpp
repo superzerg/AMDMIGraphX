c80ffc9cf41c93a63e1e72cb085a9c85e6a4881d
Cagri Eryilmaz <ceryilma@amd.com>
2021-08-16 16:09:34 +0000

    test for dilation of 2

---
+++ test/verify/test_conv3.cpp
@@ -0,0 +1,24 @@
+
+#include "verify_program.hpp"
+#include <migraphx/program.hpp>
+#include <migraphx/generate.hpp>
+#include <migraphx/make_op.hpp>
+
+struct test_conv3 : verify_program<test_conv3>
+{
+    migraphx::program create_program() const
+    {
+        migraphx::program p;
+        auto* mm = p.get_main_module();
+        auto input =
+            mm->add_parameter("x", migraphx::shape{migraphx::shape::float_type, {1, 256, 1, 1}});
+        auto weights =
+            mm->add_parameter("w", migraphx::shape{migraphx::shape::float_type, {256, 256, 3, 3}});
+        mm->add_instruction(
+            migraphx::make_op("convolution",
+                              {{"padding", {2, 2, 2, 2}}, {"stride", {1, 1}}, {"dilation", {2, 2}}, {"group", {1}}, {"padding_mode", {0}}}),
+            input,
+            weights);
+        return p;
+    }
+};
