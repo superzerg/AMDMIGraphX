f474d2d21f4578f9fdb8b5cf318b78e225b39ccd
Shucai Xiao <Shucai.Xiao@amd.com>
2021-11-09 15:05:55 -0600

    clang format

--- src/onnx/parse_lrn.cpp
+++ src/onnx/parse_lrn.cpp
@@ -9,27 +9,26 @@ namespace onnx {
 
 struct parse_lrn : op_parser<parse_lrn>
 {
-    std::vector<op_desc> operators() const
-    {
-        return {{"LRN", "lrn"}};
-    }
+    std::vector<op_desc> operators() const { return {{"LRN", "lrn"}}; }
 
     instruction_ref parse(const op_desc& opd,
                           const onnx_parser& parser,
                           onnx_parser::node_info info,
                           std::vector<instruction_ref> args) const
     {
-        auto op = parser.load(opd.op_name, info);
+        auto op   = parser.load(opd.op_name, info);
         auto& arg = args.front();
         auto type = arg->get_shape().type();
         if(type == shape::half_type)
         {
-            arg = info.add_instruction(make_op("convert", {{"target_type", shape::float_type}}), arg);
+            arg =
+                info.add_instruction(make_op("convert", {{"target_type", shape::float_type}}), arg);
         }
         auto ret = info.add_instruction(op, arg);
         if(type == shape::half_type)
         {
-            ret = info.add_instruction(make_op("convert", {{"target_type", shape::half_type}}), ret);
+            ret =
+                info.add_instruction(make_op("convert", {{"target_type", shape::half_type}}), ret);
         }
         return ret;
     }
