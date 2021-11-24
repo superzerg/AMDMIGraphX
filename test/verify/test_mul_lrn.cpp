65f4bf57b18a38f62babb8e261e39e011634f022
Shucai Xiao <Shucai.Xiao@amd.com>
2021-11-24 15:38:55 -0600

    add a unit test

---
+++ test/verify/test_mul_lrn.cpp
@@ -0,0 +1,25 @@
+
+#include "verify_program.hpp"
+#include <migraphx/program.hpp>
+#include <migraphx/generate.hpp>
+#include <migraphx/make_op.hpp>
+
+struct test_mul_lrn : verify_program<test_mul_lrn>
+{
+    migraphx::program create_program() const
+    {
+        migraphx::program p;
+        auto* mm = p.get_main_module();
+        migraphx::shape s{migraphx::shape::half_type, {1, 5, 2, 2}};
+        auto x = mm->add_parameter("x", s);
+        std::vector<float> vec(s.elements(), 500.0f);
+        auto l = mm->add_literal(migraphx::literal(s,vec));
+        auto xl = mm->add_instruction(migraphx::make_op("mul"), x, l);
+        auto y = mm->add_instruction(migraphx::make_op("relu"), xl);
+        mm->add_instruction(
+            migraphx::make_op("lrn",
+                              {{"alpha", 0.0001}, {"beta", 0.75}, {"bias", 1.0}, {"size", 5}}),
+            y);
+        return p;
+    }
+};
