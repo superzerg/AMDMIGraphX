91f635a26055e30735478b96224b0c415a1227ea
Shucai Xiao <Shucai.Xiao@amd.com>
2021-11-24 15:39:02 -0600

    clang format

--- test/verify/test_mul_lrn.cpp
+++ test/verify/test_mul_lrn.cpp
@@ -13,9 +13,9 @@ struct test_mul_lrn : verify_program<test_mul_lrn>
         migraphx::shape s{migraphx::shape::half_type, {1, 5, 2, 2}};
         auto x = mm->add_parameter("x", s);
         std::vector<float> vec(s.elements(), 500.0f);
-        auto l = mm->add_literal(migraphx::literal(s,vec));
+        auto l  = mm->add_literal(migraphx::literal(s, vec));
         auto xl = mm->add_instruction(migraphx::make_op("mul"), x, l);
-        auto y = mm->add_instruction(migraphx::make_op("relu"), xl);
+        auto y  = mm->add_instruction(migraphx::make_op("relu"), xl);
         mm->add_instruction(
             migraphx::make_op("lrn",
                               {{"alpha", 0.0001}, {"beta", 0.75}, {"bias", 1.0}, {"size", 5}}),
